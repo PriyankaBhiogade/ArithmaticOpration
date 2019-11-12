6b75de1fa4603786462ed566e66559cc09201233
vasamvishal <vasamvishal12@gmail.com>
2019-11-12 09:09:44 +0530

    updated arithmaticOperations.sh to store all opration result in dictionary

--- arithmaticOperations.sh
+++ arithmaticOperations.sh
@@ -3,16 +3,36 @@
 read -p "Enter First Value: " x
 read -p "Enter Second Value: " y
 read -p "Enter Third Value: " z
-	
-	
+declare -A calculator
+
 	function additionOfTwoNum(){
 		sum=$(( $1 + $2 ))
 		echo $sum
 	}
 	function modFunction(){
 		mod=$(( $1 % $2 ))
-	 summodOfThreeNum="$( additionOfTwoNum $(($mod)) $(($z)))"
-		echo $summodOfThreeNum
+		echo $mod
 	}
-finalResult="$( modFunction $(($x)) $(($y)))"
-	echo "( x % y + z ):: " $finalResult
+	function multiplictionFunction(){
+		mul=$(( $1 * $2 ))
+		echo $mul
+        }
+
+	function divisionFunction(){
+		div=$(( $1 / $2 ))
+		echo $div
+	}
+
+#finalResult="$( modFunction $(($x)) $(($y)))"
+	#echo "( x % y + z ):: " $finalResult
+mulResult=$(multiplictionFunction $(($y)) $(($z)))
+divResult=$(divisionFunction $(($y)) $(($z)))
+modResult=$(modFunction $(($y)) $(($z)))
+
+calculator["add"]="$( additionOfTwoNum $(($x)) $(($y)))"
+calculator["addMul"]="$(additionOfTwoNum $(($x)) $(($mulResult)))"
+calculator["adddiv"]="$(additionOfTwoNum $(($x)) $(($divResult)))"
+calculator["addmod"]="$(additionOfTwoNum $(($x)) $(($divResult)))"
+
+echo  ${calculator[@]}
+
